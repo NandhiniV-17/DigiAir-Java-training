11192c229b28d19ed31b5585fc08a8ef421777c6
NandhiniV-17 <117360180+NandhiniV-17@users.noreply.github.com>
2022-11-14 14:22:30 -0500

    Add files via upload

---
+++ studentmarkandresultcalculation/ProjectConstants.java
@@ -0,0 +1,7 @@
+package com.digiair.training.studentmarkandresultcalculation;
+
+public class ProjectConstants {
+public static final int passThresholds = 50;
+public static final int totalMarksAllSubjects = 500;
+
+}
