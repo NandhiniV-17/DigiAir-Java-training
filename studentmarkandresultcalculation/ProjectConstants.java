491ae153bfd7e598a11f2316ce845c915f552f9d
NandhiniV-17 <117360180+NandhiniV-17@users.noreply.github.com>
2022-11-15 23:16:53 -0500

    Delete studentmarkandresultcalculation directory

--- studentmarkandresultcalculation/ProjectConstants.java
+++
@@ -1,7 +0,0 @@
-package com.digiair.training.studentmarkandresultcalculation;
-
-public class ProjectConstants {
-public static final int passThresholds = 50;
-public static final int totalMarksAllSubjects = 500;
-
-}
